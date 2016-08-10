26faae301b552b83875fd609c55605a3591b4f55
Marin Roche <marinroche@gmail.com>
2016-08-10 21:52:30 +0200

    structured daemon.c

--- daemon.c
+++ daemon.c
@@ -7,6 +7,9 @@
 #include <string.h>
 #include <errno.h>
 
+void notify(char* arg) {
+        fprintf(stdout, "%s\n", arg);
+}
 
 int main(int argc, char* argv[]) {
         /* forking */
@@ -16,7 +19,7 @@ int main(int argc, char* argv[]) {
         /* client communication */
         struct sockaddr_un addr;
         int sfd, cfd, rfd;
-        char buffer[256];
+        char buffer[1024];
 
 
         /* forking daemon */
@@ -78,12 +81,23 @@ int main(int argc, char* argv[]) {
                         fprintf(stderr, "failed to accept connection.\n");
                         return EXIT_FAILURE;
                 }
-                rfd = read(cfd, buffer, 255);
+                rfd = read(cfd, buffer, 1023);
                 if (rfd < 0) {
                         fprintf(stderr, "failed to read from buffer.\n");
                         return EXIT_FAILURE;
                 }
-                fprintf(stdout, "read from buffer: %s\n", buffer);
+                process_id = fork();
+                if (process_id < 0) {
+                        fprintf(stderr, "failed to fork for notification.\n");
+                }
+
+                if (process_id == 0) {
+                        notify(buffer);
+                        return EXIT_SUCCESS;
+                }
+
+                fprintf(stdout, "created notification process with pid %d\n",
+                                process_id);
                 close(cfd);
         }
         return EXIT_SUCCESS;
