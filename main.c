5172dfd67bfe2b96c0b1d030c259e1ac21dfb1cd
Marin Roche <marinroche@gmail.com>
2016-08-10 12:57:51 +0200

    utf-8 support as far as i can tell

--- main.c
+++ main.c
@@ -4,16 +4,75 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <locale.h>
 
 int MARGIN=15;
 
-void print_help(void) {
-        printf("help coming soon\n");
+/*int utf8toXChar2b(XChar2b*, int, const char*, int);*/
+
+XFontSet getFont(Display*);
+
+void print_help(_IO_FILE*);
+
+/*
+int utf8toXChar2b(XChar2b* output_r, int outsize, const char* input, int inlen) {
+        int j, k;
+        for (j = 0; j < inlen && k < outsize; ++j) {
+                unsigned char c = input[j];
+                if (c < 128) {
+                        output_r[k].byte1 = 0;
+                        output_r[k].byte2 = c;
+                        ++k;
+                } else if (c < 0xC0) {
+                        continue;
+                } else switch (c&0xF0) {
+                        case 0xC0: case 0xD0:
+                                if (inlen < j + 1) {
+                                        return k;
+                                }
+                                output_r[k].byte1 = (c&0x1c) >> 2;
+                                ++j;
+                                output_r[k].byte2 = ((c&0x3) << 6) + (input[k]&0x3F);
+                                ++k;
+                                break;
+                        case 0xE0:
+                                if (inlen < j+2) {
+                                        return k;
+                                }
+                                ++j;
+                                output_r[k].byte1 = ((c&0xF) << 4) + ((input[j]&0x3C) >> 2);
+                                c = input[j];
+                                ++j;
+                                output_r[k].byte2 = ((c&0x3) << 6) + (input[j]&0x3F);
+                                ++k;
+                                break;
+                        case 0xFF:
+                                continue;
+                }
+        }
+        return k;
+}*/
+
+XFontSet getFont(Display* dpy) {
+        XFontSet font = NULL;
+        int nmissing;
+        char** missing;
+        char* def_string;
+
+        font = XCreateFontSet(dpy, "fixed", &missing, &nmissing, &def_string);
+        XFreeStringList(missing);
+        return font;
 }
 
-int main(int argc, char ** argv){
-        /* argv parser stuff */
-        int i;
+
+void print_help(_IO_FILE* out) {
+        fprintf(out, "help coming soon\n");
+}
+
+
+int main(int argc, char** argv){
+        /* looping stuff */
+        int i, j;
 
         /* window information */
         int width, height, screen;
@@ -21,34 +80,44 @@ int main(int argc, char ** argv){
         /* xlib stuff */
         Window win;
         XEvent ev;
-        Display *dpy;
+        Display* dpy;
         GC pen;
         XSetWindowAttributes att;
         XGCValues val;
         XColor col, dummy;
-        char * bg_col = "black";
+        char* bg_col = "black";
 
         /* text */
-        XFontStruct *font;
-        char * text = "";
+        int strlength;
         int text_width, text_height;
         int textx, texty;
+        char* text = "";
 
+        /* font */
+        int nfonts;
+        char** font_names;
+        XFontSet font;
+        XFontStruct** fonts;
+
+
+
+        /* locale */
+        setlocale(LC_ALL, getenv("LANG"));
 
         /* argv parser */
 
         for (i = 1; i < argc; ++i) {
                 if (strcmp(argv[i], "-h") == 0
                                 || strcmp(argv[i], "--help") == 0) {
-                        print_help();
+                        print_help(stdout);
                         return EXIT_SUCCESS;
                 } else if (strcmp(argv[i], "-b") == 0
                                 || strcmp(argv[i], "--background") == 0) {
                         if (++i < argc) {
                                 bg_col = argv[i];
                         } else {
-                                printf("usage:\n");
-                                print_help();
+                                fprintf(stderr, "usage:\n");
+                                print_help(stderr);
                                 return EXIT_FAILURE;
                         }
                 } else {
@@ -56,8 +125,6 @@ int main(int argc, char ** argv){
                 }
         }
 
-
-        fprintf("color: %s\n", bg_col);
 
         /* display setup */
 
@@ -71,11 +138,17 @@ int main(int argc, char ** argv){
         /* pen setup */
         val.foreground = XWhitePixel(dpy, screen);
 
-        /* font setup */
-        font = XLoadQueryFont(dpy, "fixed");
-        val.font = font->fid;
-        text_width = XTextWidth(font, text, strlen(text));
-        text_height = font->ascent;
+        /* text setup */
+        strlength = strlen(text);
+        font = getFont(dpy);
+        text_width = Xutf8TextEscapement(font, text, strlength);
+
+        text_height = 0;
+        nfonts = XFontsOfFontSet(font, &fonts, &font_names);
+        for(j = 0; j < nfonts; ++j) {
+                if (text_height < fonts[j]->ascent) text_height = fonts[j]->ascent;
+        }
+
 
         /* window setup */
 
@@ -98,13 +171,14 @@ int main(int argc, char ** argv){
 
 
         /* pen creation */
-        pen = XCreateGC(dpy, win, GCForeground|GCFont, &val);
+        pen = XCreateGC(dpy, win, GCForeground, &val);
+
 
 
         /* text writing */
         textx = (width - text_width)/2;
-        texty = (height + font->ascent)/2;
-        XDrawString(dpy, win, pen, textx, texty, text, strlen(text));
+        texty = (height + text_height)/2;
+        Xutf8DrawString(dpy, win, font, pen, textx, texty, text, strlength);
 
 
         /* waiting for the end */
