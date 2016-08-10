54a43f8a7440a366d4e8ce9b29acc44e4a712420
Marin Roche <marinroche@gmail.com>
2016-08-10 22:11:47 +0200

    client working, only lacks help

--- client.c
+++ client.c
@@ -5,10 +5,46 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int main(void) {
+void print_help(_IO_FILE*);
+
+
+void print_help(_IO_FILE* out) {
+        fprintf(out, "help coming soon\n");
+}
+
+int main(int argc, char** argv) {
+        /* args */
+        int i;
+        char* bg_col = "black";
+        char* text;
+        char* sarg;
+
+        /* server connection */
         struct sockaddr_un addr;
         int sfd;
 
+        /* parsing args */
+        for (i = 1; i < argc; ++i) {
+                if (strcmp(argv[i], "-h") == 0
+                                || strcmp(argv[i], "--help") == 0) {
+                        print_help(stdout);
+                        return EXIT_SUCCESS;
+                } else if (strcmp(argv[i], "-b") == 0
+                                || strcmp(argv[i], "--background") == 0) {
+                        if (++i < argc) {
+                                bg_col = argv[i];
+                        } else {
+                                fprintf(stderr, "usage:\n");
+                                print_help(stderr);
+                                return EXIT_FAILURE;
+                        }
+                } else {
+                        text = argv[i];
+                }
+        }
+
+
+        /* connecting to server */
         sfd = socket(AF_UNIX, SOCK_STREAM, 0);
 
         if (sfd < 0) {
@@ -25,7 +61,19 @@ int main(void) {
                 return EXIT_FAILURE;
         }
 
-        write(sfd, "mensch!", strlen("mensch!"));
+        /* sending the command */
+        sarg = malloc(strlen(bg_col) + strlen(text) + 2);
+
+        if (!sarg) {
+                fprintf(stderr, "failed to allocate memory.\n");
+                return EXIT_FAILURE;
+        }
+
+        strcpy(sarg, bg_col);
+        strcat(sarg, " ");
+        strcat(sarg, text);
+
+        write(sfd, sarg, strlen(sarg) + 1);
 
         close(sfd);
 
